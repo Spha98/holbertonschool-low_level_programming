eca1c58ac6fa3587894bf4214a7468fff4289dde
Spha98 <ernestspha98@gmail.com>
2022-02-04 04:57:13 -0800

    C - Variables, if,else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,7 +10,8 @@ int main(void)
 {
 	char d;
 
-	for (d = 'a';d <= 'z'; d++)
+	for (d = 'a'; d <= 'z'; d++)
+
 	    putchar(d);
 
 	putchar('\n');
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -15,7 +15,5 @@ int main(void)
 	}
 
 	putchar('\n');
-
 	return (0);
- 
 }
