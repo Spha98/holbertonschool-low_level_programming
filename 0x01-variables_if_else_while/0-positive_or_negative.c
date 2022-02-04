4e8429addd525bcb5a280441d706b5e681c8838d
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-02-05 00:20:30 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,16 +10,15 @@
 int main(void)
 {
 	int n;
-	
+	/*Method*/
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+	/*if stetement*/
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-	
 	return (0);
 }
