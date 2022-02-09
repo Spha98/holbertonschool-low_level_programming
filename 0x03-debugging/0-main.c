ccd30adbbc1ff7460fac69036cf47c825f4421ef
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-02-09 12:31:11 +0200

    C - Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
-	int i = 0;
-	/*method*/
+	int i;
+
+	i = 0;
+
 	positve_or_negative(i);
+
 	return (0);
 }
