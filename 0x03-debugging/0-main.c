3279753a6e7d14955d2b3c97377ca4294b62b864
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-02-09 13:42:34 +0200

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
 	int i;
 
 	i = 0;
-	positve_or_negative(i);
+	positive_or_negative(i);
 
 	return (0);
 }
