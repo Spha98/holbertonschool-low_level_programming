a29b7d94a5049b775d7e2a012bfd4d523665501c
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-02-10 12:04:05 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - Check if a letter is supper
- * @x: The number to be checked
- *
- * Return: 1 for upper letter or 0 for any else
- */
+  * _isupper - Check if a letter is upper
+  * @x: The number to be checked
+  *
+  * Return: 1 for upper letter or 0 for any else
+  */
 int _isupper(int x)
 {
 	if (x >= 65 && x <= 90)
