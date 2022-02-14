91b1eadacb861ea6c4036518f17100ba920b017d
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-02-14 13:39:26 +0200

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strcpy - Copy a string
- * @dest: Destination value
- * @srs: Source value
- *
- * Return: the pointer to dest
- */
+  * _strcpy - Copy a string
+  * @dest: Destination value
+  * @src: Source value
+  *
+  * Return: the pointer to dest
+  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
