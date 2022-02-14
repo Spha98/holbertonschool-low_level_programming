e222f72157f528a7fc87b99375a0df1dfe26ec02
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-02-14 11:34:55 +0200

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: the string to be treated
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+General
+- what are pointers and how to use them
+- What are arrays and how ti use them
+- What are the differences between pointers abd arrays
+- how to use strings and how to manipulate them
+- Scope of variables
