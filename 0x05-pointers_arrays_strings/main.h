c3cef02e8785b704a74ac0e00c5ca33063384f67
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-02-14 11:54:50 +0200

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void sawp_int(int *a, int *b);
 int _strlen(char *s);
-int _puts(char *str);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
