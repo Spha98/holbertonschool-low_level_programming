29545485e7658008215628e51ecd8494cbc51c33
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-02-17 09:43:12 +0200

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,6 +9,7 @@
  *
  * Return: void
  */
+
 void print_line(char *c, int s, int l)
 {
 	int j, k;
@@ -16,15 +17,15 @@ void print_line(char *c, int s, int l)
 	for (j = 0; j <= 9; j++)
 	{
 		if (j <= s)
-			printf("%02x", c[1 * 10 + j]);
+			printf("%02x", c[l * 10 + j]);
 		else
-			printf(" ");
+			printf("  ");
 		if (j % 2)
 			putchar(' ');
 	}
 	for (k = 0; k <= s; k++)
 	{
-		if (c[l * 10 + k] > 31 && c[l * 10 +k] < 127)
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 			putchar(c[l * 10 + k]);
 		else
 			putchar('.');
