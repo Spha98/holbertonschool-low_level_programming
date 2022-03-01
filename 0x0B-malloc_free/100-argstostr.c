063858494b2274ee561d03ac8a072045efb5bb91
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-03-01 16:02:07 +0200

    C - malloc, free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.c>
+
+/**
+ *argstostr - convert the params passed to the program to string
+ *@ac: the argument count
+ *@av: the argument vector
+ *
+ * Return: ...
+ */
+char *argstostr(int ac, char **av)
+{
+	int ch = 0, i = 0, j = 0, k = 0;
+	char *s;
+
+	if (ac == 0 || av == 0)
+		return (NULL);
+
+	while (i < ac)
+	{
+		while (av[i][j])
+		{
+			ch++;
+			j++;
+		}
+
+		j = 0;
+		i++;
+	}
+
+	s = melloc((sizeof(char) * ch) + ac + 1);
+
+	i = 0;
+	while (av[i])
+	{
+		while (av[i][j])
+		{
+			s[k] = av[i][j];
+			k++;
+			j++;
+		}
+
+		s[k] = '\n';
+
+		j = 0;
+		k++;
+		i++;	
+	}
+
+	k++;
+	s[k] = '\0';
+	return (s);
+}
