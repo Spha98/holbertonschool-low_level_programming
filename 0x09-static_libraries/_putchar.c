393ea4e76992bad553b6f0eac1735b9daeb23f48
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-03-02 14:48:24 +0200

    Update _putchar.c

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+[14:47] Mpho Kutoane
+#include <unistd.h>/**
+* _putchar - writes the character c to std out.
+* @c: the character to print
+* Return: 1 on success.
+* on error, -1
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
