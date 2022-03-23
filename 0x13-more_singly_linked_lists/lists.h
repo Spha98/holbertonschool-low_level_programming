81b5d30d4be2d95bed698b7c4a9c3e2bb064578d
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-03-23 10:52:25 +0200

    adding main file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _S_LINK_LISTS_
 #define _S_LINK_LISTS_
 
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -27,5 +28,8 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* _S_LINK_LISTS_*/
