3d969f6f6eb41b636bb3b326f29e0ed8bb148081
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-05-10 15:52:54 +0200

    fixing

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -p .. https://github.com/Spha98/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
